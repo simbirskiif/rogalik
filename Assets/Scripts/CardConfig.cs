253eaad05f91300128577b1ee5e77be6fc04ff13
simbirskiif <simbirskiif@gmail.com>
2026-03-24 21:16:32 +0400

    Shader HoloCardShader

--- Assets/Scripts/CardConfig.cs
+++ Assets/Scripts/CardConfig.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class CardConfig : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
